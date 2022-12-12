dff6965f912b188b1aeb4758134a0cf287eb0eae
Lion Black <lionblack1079@gmail.com>
2022-12-12 15:43:57 +0100

    v 1.0.1

--- src/js/partials/weather.js
+++ src/js/partials/weather.js
@@ -8,7 +8,6 @@ window.onload = function () {
   const humidity = document.querySelector("#humidity");
   const searchSity = document.querySelector("#search-form");
   const currentDate = new Date();
-  console.log("t");
   const options = {
     weekday: "long",
     month: "short",
@@ -36,18 +35,26 @@ window.onload = function () {
 
     const apiKeys = process.env.apiKey;
     console.log(apiKeys);
-    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityForRequest}&units=metric`;
+
+    const apiURL = `http://api.openweathermap.org/data/2.5/forecast?q=${cityForRequest}&units=metric`;
     axios.get(`${apiURL}&appid=${apiKeys}`).then(renderWeatherInfoRequest);
   }
   function renderWeatherInfoRequest(apiObj) {
-    const avrTempRequest = apiObj.data.main.temp;
-    const windSpeedRequest = apiObj.data.wind.speed;
-    const windDirectionRequest = apiObj.data.wind.deg;
-    const pressureRequest = apiObj.data.main.pressure;
-    const humidityRequest = apiObj.data.main.humidity;
-    let cityForRequest = localStorage.getItem("city");
-    windDirection(windDirectionRequest);
+    console.log(apiObj.data.list);
+    console.log(currentDate);
+
+    tomorrowWeather(apiObj);
+
     time.innerText = currentTime;
+    const weatherArray = apiObj.data.list;
+    console.log(weatherArray[0]);
+    const avrTempRequest = weatherArray[0].main.temp;
+    const windSpeedRequest = weatherArray[0].wind.speed;
+    const windDirectionRequest = weatherArray[0].wind.deg;
+    const pressureRequest = weatherArray[0].main.pressure;
+    const humidityRequest = weatherArray[0].main.humidity;
+    const cityForRequest = localStorage.getItem("city");
+    windDirection(windDirectionRequest);
     cityName.innerText = cityForRequest;
     mainTemperature.innerText = Math.round(avrTempRequest);
     wind.innerText = `Wind: ${Math.round(
@@ -57,9 +64,43 @@ window.onload = function () {
     humidity.innerText = `Humidity: ${humidityRequest}%`;
 
     city.value = "";
-    console.log(humidityRequest);
-    console.log(apiObj.data);
   }
+  function tomorrowWeather(apiObj) {
+    const weatherArray = apiObj.data.list;
+    let tomorrow = new Date(
+      currentDate.getUTCFullYear(),
+      currentDate.getUTCMonth(),
+      currentDate.getUTCDate() + 1
+    );
+    let afterTomorrow = new Date(
+      currentDate.getUTCFullYear(),
+      currentDate.getUTCMonth(),
+      currentDate.getUTCDate() + 2
+    );
+    let unixTimeTomorrow = Math.floor(+tomorrow) / 1000;
+    let unixTimeAfterTomorrow = Math.floor(+afterTomorrow) / 1000;
+
+    let tomorrowDayWeatherArray = weatherArray.filter(
+      (item) => item.dt > unixTimeTomorrow && item.dt < unixTimeAfterTomorrow
+    );
+    console.log(tomorrowDayWeatherArray);
+    let tomorrowArr = [];
+    tomorrowDayWeatherArray.forEach((element) => {
+      let result = element.main.temp;
+      tomorrowArr.push(result);
+
+      Math.max(element.main.temp);
+    });
+    console.log(tomorrowArr);
+    let tomorrowArrNight = tomorrowArr.splice(0, 4);
+    console.log(tomorrowArrNight);
+    tomorrowArrDay = tomorrowArr;
+    console.log(tomorrowArrDay);
+    const nightTemp = Math.min.apply(null, tomorrowArrNight);
+    const dayTemp = Math.max.apply(null, tomorrowArrDay);
+    console.log(dayTemp, nightTemp);
+  }
+
   function windDirection(wind) {
     return (direction =
       (wind >= 338 && wind <= 360) || (wind >= 0 && wind < 23)
