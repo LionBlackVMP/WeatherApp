92c581e56637a83be0544e8515e484cc59b057df
Lion Black <lionblack1079@gmail.com>
2022-12-15 10:51:59 +0100

    v. 1.0.3

--- src/js/partials/weather.js
+++ src/js/partials/weather.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+  const apiKeys = process.env.apiKey;
   const time = document.querySelector("#time");
   const city = document.querySelector("#city");
   const cityName = document.querySelector("#city-name");
@@ -7,7 +8,15 @@ window.onload = function () {
   const pressure = document.querySelector("#pressure");
   const humidity = document.querySelector("#humidity");
   const searchSity = document.querySelector("#search-form");
+  const secondDay = document.querySelector("#secondDay");
+  const thirdDay = document.querySelector("#thirdDay");
+  const fourthDay = document.querySelector("#fourthDay");
+  const fifthDay = document.querySelector("#fifthDay");
+  const celsius = document.querySelector(".celsius");
+  const fahrenheit = document.querySelector(".fahrenheit");
   const currentDate = new Date();
+  const MetricUnitOfMeasurement = "metric";
+
   const options = {
     weekday: "long",
     month: "short",
@@ -21,88 +30,101 @@ window.onload = function () {
     if (event) {
       event.preventDefault();
     }
+    let unitsOfMeasurement = localStorage.getItem("unitsOfMeasurement");
+    if (!unitsOfMeasurement) {
+      unitsOfMeasurement = MetricUnitOfMeasurement;
+    }
     let cityForRequest = city.value.trim();
-
     if (cityForRequest) {
       let cityForRequestLetterCase =
         cityForRequest[0].toUpperCase() + cityForRequest.slice(1).toLowerCase();
-
       localStorage.setItem("city", cityForRequestLetterCase);
     }
     if (!cityForRequest) {
       cityForRequest = localStorage.getItem("city");
     }
 
-    const apiKeys = process.env.apiKey;
-    console.log(apiKeys);
-
-    const apiURL = `http://api.openweathermap.org/data/2.5/forecast?q=${cityForRequest}&units=metric`;
-    axios.get(`${apiURL}&appid=${apiKeys}`).then(renderWeatherInfoRequest);
+    const apiURL = `http://api.openweathermap.org/data/2.5/forecast?q=${cityForRequest}&units=${unitsOfMeasurement}`;
+    axios.get(`${apiURL}&appid=${apiKeys}`).then(renderToDayWeatherInfoRequest);
   }
-  function renderWeatherInfoRequest(apiObj) {
-    console.log(apiObj.data.list);
-    console.log(currentDate);
 
-    tomorrowWeather(apiObj);
-
-    time.innerText = currentTime;
+  function renderToDayWeatherInfoRequest(apiObj) {
     const weatherArray = apiObj.data.list;
-    console.log(weatherArray[0]);
     const avrTempRequest = weatherArray[0].main.temp;
-    const windSpeedRequest = weatherArray[0].wind.speed;
     const windDirectionRequest = weatherArray[0].wind.deg;
     const pressureRequest = weatherArray[0].main.pressure;
     const humidityRequest = weatherArray[0].main.humidity;
     const cityForRequest = localStorage.getItem("city");
+    let windSpeedRequest = weatherArray[0].wind.speed;
+    console.log(windSpeedRequest);
+    let unitsOfMeasurement = localStorage.getItem("unitsOfMeasurement");
+    windSpeedRequest =
+      unitsOfMeasurement == "metric"
+        ? String(Math.round(windSpeedRequest * 3.6) + " km/h")
+        : String(Math.round(windSpeedRequest) + " mil/h");
+    console.log(weatherArray[0]);
+    time.innerText = currentTime;
+    let dayNumber = 1;
+    for (dayNumber; dayNumber < 5; dayNumber++) {
+      renderWeatherInfoOnNextDays(dayNumber, weatherArray);
+    }
     windDirection(windDirectionRequest);
     cityName.innerText = cityForRequest;
     mainTemperature.innerText = Math.round(avrTempRequest);
-    wind.innerText = `Wind: ${Math.round(
-      windSpeedRequest
-    )} km/h, ${direction} `;
+    wind.innerText = `Wind: ${windSpeedRequest}, ${direction} `;
     pressure.innerText = `Pressure: ${pressureRequest}`;
     humidity.innerText = `Humidity: ${humidityRequest}%`;
 
     city.value = "";
   }
-  function tomorrowWeather(apiObj) {
-    const weatherArray = apiObj.data.list;
-    let tomorrow = new Date(
-      currentDate.getUTCFullYear(),
-      currentDate.getUTCMonth(),
-      currentDate.getUTCDate() + 1
-    );
-    let afterTomorrow = new Date(
-      currentDate.getUTCFullYear(),
-      currentDate.getUTCMonth(),
-      currentDate.getUTCDate() + 2
-    );
-    let unixTimeTomorrow = Math.floor(+tomorrow) / 1000;
-    let unixTimeAfterTomorrow = Math.floor(+afterTomorrow) / 1000;
 
-    let tomorrowDayWeatherArray = weatherArray.filter(
-      (item) => item.dt > unixTimeTomorrow && item.dt < unixTimeAfterTomorrow
+  function renderWeatherInfoOnNextDays(dayNumber, weatherArray) {
+    const dayFirst = new Date(
+      currentDate.getUTCFullYear(),
+      currentDate.getUTCMonth(),
+      currentDate.getUTCDate() + dayNumber
     );
-    console.log(tomorrowDayWeatherArray);
-    let tomorrowArr = [];
-    tomorrowDayWeatherArray.forEach((element) => {
+    const daySecond = new Date(
+      currentDate.getUTCFullYear(),
+      currentDate.getUTCMonth(),
+      currentDate.getUTCDate() + dayNumber + 1
+    );
+    const options = {
+      weekday: "long",
+    };
+    const day = dayFirst.toLocaleDateString("en-GB", options);
+    const unixTimedayFirst = Math.floor(+dayFirst) / 1000;
+    const unixTimeSecond = Math.floor(+daySecond) / 1000;
+    const oneDayWeatherInfo = weatherArray.filter(
+      (item) => item.dt > unixTimedayFirst && item.dt < unixTimeSecond
+    );
+
+    let oneDayTemp = [];
+    oneDayWeatherInfo.forEach((element) => {
       let result = element.main.temp;
-      tomorrowArr.push(result);
+      oneDayTemp.push(result);
 
       Math.max(element.main.temp);
     });
-    console.log(tomorrowArr);
-    let tomorrowArrNight = tomorrowArr.splice(0, 4);
-    console.log(tomorrowArrNight);
-    tomorrowArrDay = tomorrowArr;
-    console.log(tomorrowArrDay);
-    const nightTemp = Math.min.apply(null, tomorrowArrNight);
-    const dayTemp = Math.max.apply(null, tomorrowArrDay);
-    console.log(dayTemp, nightTemp);
-  }
+    let oneDayTempNight = oneDayTemp.splice(0, 4);
+    oneDayTempDay = oneDayTemp;
+    let nightTemp = Math.min.apply(null, oneDayTempNight);
+    let dayTemp = Math.max.apply(null, oneDayTempDay);
+    nightTemp = Math.round(nightTemp);
+    dayTemp = Math.round(dayTemp);
 
+    dayNumber === 1
+      ? (secondDay.innerText = `${day} \n ${dayTemp}° ${nightTemp}°  `)
+      : dayNumber === 2
+      ? (thirdDay.innerText = `${day} \n ${dayTemp}° ${nightTemp}°  `)
+      : dayNumber === 3
+      ? (fourthDay.innerText = `${day} \n ${dayTemp}° ${nightTemp}°  `)
+      : dayNumber === 4
+      ? (fifthDay.innerText = `${day} \n ${dayTemp}° ${nightTemp}°  `)
+      : console.log("error");
+  }
   function windDirection(wind) {
-    return (direction =
+    direction =
       (wind >= 338 && wind <= 360) || (wind >= 0 && wind < 23)
         ? "North"
         : wind >= 23 && wind < 68
@@ -117,8 +139,23 @@ window.onload = function () {
         ? "South-West"
         : wind >= 248 && wind < 293
         ? "West"
-        : "North-West");
+        : "North-West";
   }
   processWeatherInfoRequest();
   searchSity.addEventListener("submit", processWeatherInfoRequest);
+  celsius.addEventListener("click", (e) => {
+    if (e.target.className == "celsius") {
+      const unitsOfMeasurement = "metric";
+      localStorage.setItem("unitsOfMeasurement", unitsOfMeasurement);
+
+      processWeatherInfoRequest(event, unitsOfMeasurement);
+    }
+  });
+  fahrenheit.addEventListener("click", (e) => {
+    if (e.target.className == "fahrenheit") {
+      const unitsOfMeasurement = "imperial";
+      localStorage.setItem("unitsOfMeasurement", unitsOfMeasurement);
+      processWeatherInfoRequest(event, unitsOfMeasurement);
+    }
+  });
 };
